390d393b58058e56c05851cbcc08148bc9957c18
byunseonuk <byunseonuk@naver.com>
2020-02-29 10:55:07 +0900

    Delete MovieDAO.java

--- 4주차/datavisualization/src/main/java/com/visualization/dao/MovieDAO.java
+++
@@ -1,9 +0,0 @@
-package com.visualization.dao;
-
-import java.util.List;
-
-import com.visualization.vo.MovieVO;
-
-public interface MovieDAO {
-	public List<MovieVO> selectMovie() throws Exception;
-}
