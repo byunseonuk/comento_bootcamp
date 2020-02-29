2e8071266279bbd667f8b1d82e2334ef9865763d
byunseonuk <byunseonuk@naver.com>
2020-02-29 10:54:45 +0900

    Delete MovieService.java

--- 4주차/datavisualization/src/main/java/com/visualization/service/MovieService.java
+++
@@ -1,9 +0,0 @@
-package com.visualization.service;
-
-import java.util.List;
-
-import com.visualization.vo.MovieVO;
-
-public interface MovieService {
-	public List<MovieVO> selectMovie() throws Exception;
-}
